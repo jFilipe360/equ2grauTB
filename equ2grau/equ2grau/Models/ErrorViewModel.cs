9fbdfc2a5d59cf8bc287cd2fcdb4c8cda29db93c
Joao Correia <aluno21302@ipt.pt>
2023-03-16 15:28:01 +0000

    Criação do projeto

---
+++ equ2grau/equ2grau/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace equ2grau.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
